02c3d7bea49124dbf0d0ff46c487c81bc3a4593f
https://github.com/nurseincode <nurseincode@gmail.com>
2025-10-18 20:40:32 +1100

    Writing files done

--- csv_file.py
+++ csv_file.py
@@ -21,13 +21,38 @@ with open('euro_cup_matches.csv') as file:
 print(f'Total goals in tournament so far: {total_goals}')
 
 # Writing CSV files
-with open('new_matches.csv', 'w') as file: # Reusing file 
-    writer = csv.DictWriter(file, ['Team A', 'Team B', 'Score A', 'Score B', 'Date']) # Defining column headers(field names) explicitly
-    writer.writeheader() # Writes the first row using above fieldnames as col headers
-    writer.writerow({
-        'Team A': 'Portugal',
-        'Team B': 'Spain',
+# with open('new_matches.csv', 'w') as file: # Reusing file 
+#     writer = csv.DictWriter(file, ['Team A', 'Team B', 'Score A', 'Score B', 'Date']) # Defining column headers(field names) explicitly
+#     writer.writeheader() # Writes the first row using above fieldnames as col headers
+#     writer.writerow({
+#         'Team A': 'Portugal',
+#         'Team B': 'Spain',
+#         'Score A': 1,
+#         'Score B': 1,
+#         'Date': '2024-04-15'
+#     })
+
+# Using writerrows to call dicts in a list
+
+matches = [ 
+    {
+        'Team A': 'Spain',
+        'Team B': 'Italy',
+        'Score A': 1,
+        'Score B': 1,
+        'Date': '2024-04-20'
+    },
+    {
+        'Team A': 'France',
+        'Team B': 'Germany',
         'Score A': 1,
         'Score B': 1,
         'Date': '2024-04-15'
-    })
+    }
+]
+with open('new_matches.csv', 'w') as file: 
+    # writer = csv.DictWriter(file, ['Team A', 'Team B', 'Score A', 'Score B', 'Date']) 
+    writer = csv.DictWriter(file, matches[0].keys()) # No redundancy 
+    writer.writeheader() 
+    writer.writerows(matches) # Calling the matches from the list
+      
